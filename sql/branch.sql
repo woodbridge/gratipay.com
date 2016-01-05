bf6d6b46c87347f511347ed95ee9286e118c0fed
Chad Whitacre <chad@zetaweb.com>
2016-01-04 22:54:19 -0500

    Correct the payin minimum

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,4 +1,4 @@
 BEGIN;
     UPDATE payment_instructions SET due = 0 WHERE amount = 0 AND due != 0;
-    UPDATE payment_instructions SET due = floor(9.81/(amount)) * amount WHERE due > 9.81;
+    UPDATE payment_instructions SET due = floor(9.41/(amount)) * amount WHERE due > 9.41;
 END;
