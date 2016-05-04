9162ef72a721cef02b04ef51e78e1cacf8a655c8
Chad Whitacre <chad@zetaweb.com>
2016-05-03 22:17:17 -0400

    stub out test for payroll during payday

--- tests/py/test_billing_payday.py
+++ tests/py/test_billing_payday.py
@@ -569,6 +569,13 @@ class TestPayin(BillingHarness):
         assert filename.endswith('_payments.csv')
         os.unlink(filename)
 
+
+class TestPayroll(BillingHarness):
+
+    def test_payday_distributes_payroll(self):
+        assert 0
+
+
 class TestNotifyParticipants(EmailHarness):
 
     def test_it_notifies_participants(self):
