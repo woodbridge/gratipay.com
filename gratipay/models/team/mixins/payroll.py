a082cd09bcb4eb69d49d5d527c9e982751464531
Chad Whitacre <chad@zetaweb.com>
2016-05-03 22:02:24 -0400

    stub out payroll plumbing

--- gratipay/models/team/mixins/payroll.py
+++ gratipay/models/team/mixins/payroll.py
@@ -5,3 +5,15 @@ class PayrollMixin(object):
     """This mixing provides management of payroll for
     :py:class:`~gratipay.models.team.Team` objects.
     """
+
+
+    def add_to_payroll(self, participant):
+        """Add a participant to the team's payroll.
+        """
+        raise NotImplementedError
+
+
+    def remove_from_payroll(self, participant):
+        """Remove a participant from the team's payroll.
+        """
+        raise NotImplementedError
--- tests/py/test_team_payroll.py
+++ tests/py/test_team_payroll.py
@@ -9,3 +9,22 @@ class Tests(Harness):
     def test_team_object_subclasses_payroll_mixin(self):
         enterprise = self.make_team('The Enterprise')
         assert isinstance(enterprise, mixins.Payroll)
+
+
+    # atp - add_to_payroll
+
+    def test_atp_adds_to_payroll(self):
+        enterprise = self.make_team('The Enterprise')
+        crusher = self.make_participant('crusher')
+        enterprise.add_to_payroll(crusher)
+        assert crusher in enterprise
+
+
+    # rfp - remove_from_payroll
+
+    def test_rfp_removes_from_payroll(self):
+        enterprise = self.make_team('The Enterprise')
+        crusher = self.make_participant('crusher')
+        enterprise.add_to_payroll(crusher)
+        enterprise.remove_from_payroll(crusher)
+        assert crusher not in enterprise
